5a5ce7ded16fde92d84143c47f1cf7534e2ef487
EduardGuilherme <eduguilherme48@gmail.com>
2023-07-14 13:59:06 -0300

    Atualizando arquivos

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -9,8 +9,7 @@ import br.com.banco.models.Transferencia;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long>{
 	List<Transferencia> findAll();
-	/*@Query("SELECT t FROM Transferencia t WHERE t.dataTransferencia BETWEEN :startDateTime AND :endDateTime")
-    List<Transferencia> findByPeriodo(LocalDateTime startDateTime, LocalDateTime endDateTime);*/
+	
 	List<Transferencia> findByDataTransferenciaBetween(LocalDateTime inicio, LocalDateTime fim);
 
     List<Transferencia> findByNomeOperadorTransacao(String nomeOperadorTransacao);
